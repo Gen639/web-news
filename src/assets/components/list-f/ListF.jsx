56cfcd5f82acef082257d4eaa3cc27b2194816c7
Gen639 <evgeny.goncharov.pro@gmail.com>
2024-02-07 20:49:39 +0100

    Preparing the /list to show the news from the api

--- src/assets/components/list-f/ListF.jsx
+++ src/assets/components/list-f/ListF.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const ListF = () => {
-  return <div>ListF</div>;
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    getNews();
+  }, []);
+  const getNews = async () => {
+    try {
+      const response = await axios.get("here is the api");
+      setNews(response.data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>ListF</h2>
+      <div></div>
+    </div>
+  );
 };
 
 export default ListF;
