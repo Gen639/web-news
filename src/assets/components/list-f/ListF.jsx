6bee7a4e6c5bf0be47dd7e8f59a390f43a40b8ea
Evgeny Goncharov <evgeny.goncharov.pro@gmail.com>
2024-02-08 18:41:16 +0100

    Added List page, 10 pieces of news displayed

--- src/assets/components/list-f/ListF.jsx
+++ src/assets/components/list-f/ListF.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./ListF.css";
 
 const ListF = () => {
   const [news, setNews] = useState([]);
@@ -9,8 +10,14 @@ const ListF = () => {
   }, []);
   const getNews = async () => {
     try {
-      const response = await axios.get("here is the api");
-      setNews(response.data.results);
+      const response = await axios.get(
+        "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=retail&api-key=sWMost9WQwGwn1pHQo9mXGxvksyhUsDu"
+      );
+      console.log(response);
+      const articles = response.data.response.docs;
+      console.log(articles, "1. this is the articles array");
+      setNews(articles);
+      console.log(news, `2. this is the news array`);
     } catch (error) {
       console.error(error);
     }
@@ -19,7 +26,15 @@ const ListF = () => {
   return (
     <div>
       <h2>ListF</h2>
-      <div></div>
+      <div className="main-container">
+        {" "}
+        {news.map((piece, index) => (
+          <div className="div-container" key={index}>
+            <h3>{piece.headline.main}</h3>
+            <p>{piece.abstract}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
