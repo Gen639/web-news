867d645ca7923dd561d26d46a39123f46fd0dd78
Gen639 <evgeny.goncharov.pro@gmail.com>
2024-02-12 19:40:07 +0100

    Added Vite dotenv functionality

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -14,7 +14,9 @@ export const GlobalProvider = ({ children }) => {
   const getNews = async () => {
     try {
       const response = await axios.get(
-        `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=retail&api-key=xxx`
+        `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=retail&api-key=${
+          import.meta.env.VITE_APIKEY
+        }`
       );
 
       const locallyStoredNews =
